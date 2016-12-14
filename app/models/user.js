cf7779354c840d2a75dc91bb323a8c4bdbb5fb6f
Erin Witmer <ecwitmer@gmail.com>
2016-12-13 21:06:31 -0500

    fixes to tests

--- app/models/user.js
+++ app/models/user.js
@@ -1,21 +1,23 @@
 var mongoose = require('mongoose');
 
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-};
-
 var UserSchema = new mongoose.Schema({
     category        : String,
     bookLikes       : [String],
-    email: { 
-	    type: String,
-	    trim: true,
-	    lowercase: true,
-	    unique: true,
-	    required: true,
-	    validate: [ validateEmail, 'invalid email' ]
-	},
+    
+	email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: [true, 'User email required'],
+        validate: {
+         	validator: function(v) {
+            	return /.+\@.+\..+/.test(v);
+         	},
+          	message: '{VALUE} is not a valid email'
+        	}
+
+     },
     firstName		: String,
     lastName		: String
 
--- test/test-user.js
+++ test/test-user.js
@@ -1,43 +1,35 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var mongoose = require("mongoose");
 
-const should = chai.should();
+server = require('../app/app');
+var User = require('../app/models/user');
 
+var should = chai.should();
 chai.use(chaiHttp);
 
-let server = require('../app/app');
-let User = require('../app/models/user')
 
-
-describe('Users', function() {
-
-  User.collection.drop();
-
-  beforeEach(function(done){
+beforeEach(function(){
     var newUser = new User({
       category : "baby",
       bookLikes : ["babyA", "babyC", "babyE"],
       email : "test@user.com",
       firstName   : "Test",
       lastName    : "User" });
-
     newUser.save(function(err) {
       done();
     });
   });
-  
-  afterEach(function(done){
-    User.collection.drop();
-    done();
-  });
 
-/*  it('should delete a user on DELETE', function(done) {
-  chai.request(server)
+describe('Users', function() {
+
+  it('should delete a user on DELETE', function(done) {
+    chai.request(server)
     .delete('/user/test@user.com')
     .end(function(err, res){
       res.should.have.status(201);
-      done();
       });
+    done();
     }); 
 
   it('should edit a user on PUT', function(done) {
@@ -46,72 +38,7 @@ describe('Users', function() {
     .send()
     .end(function(err, res){
       res.should.have.status(201);
-      done();
       });
-    }); */
-
+    done();
+    }); 
 });
-
-
-/*describe('User', function() {
-  
-  it('should save a user to the database', function(done){
-
-    const user = {
-      category : "baby",
-      bookLikes : ["babyA", "babyC", "babyE"],
-      email : "test@user.com",
-      firstName   : "Test",
-      lastName    : "User" };
-
-    
-
-  })
-
-  it('should login a user in the database', function(done) {
-
-
-
-
-  });
-
-  it('should update a user book and category choices on PUT', function(done) {
-    
-    const user = {
-      category : "baby",
-      bookLikes : ["babyA", "babyC", "babyE"],
-      email : "test@user.com",
-      firstName   : "Test",
-      lastName    : "User" };
-
-
-    chai.request(server)
-      .put('/user/test@user.com')
-          .send(updated)
-          .end(function(err, res) {
-              res.should.have.status(201);
-              res.body.should.be.a('object');
-
-          });
-      done();
-  });
-
-
-  it('should recipes on DELETE', function(done) {
-    chai.request(server)
-      // first have to get recipes so have `id` for one we want
-      // to delete. Note that once we're working with databases later
-      // in this course, we'll be able get the `id` of an existing instance
-      // directly from the database, which will allow us to isolate the DELETE
-      // logic under test from our GET interface
-      .get('/recipes')
-      .end(function(err, res) {
-        chai.request(server)
-            .delete(`/recipes/${res.body[0].id}`)
-            .end(function(err, res) {
-                res.should.have.status(204);
-            });
-      });
-      done();
-  });
-  */
