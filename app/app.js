215b73d706a379aa751b9414a3b76c17541e9a6e
Erin Witmer <ecwitmer@gmail.com>
2016-12-13 19:04:36 -0500

    update to package.json

--- app/app.js
+++ app/app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-require('dotenv').config()
-
-
 var app = express();
 
 app.set('port', process.env.PORT || 3000 );
